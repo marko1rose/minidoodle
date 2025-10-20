ed155f7fa6b6c626ad9de622c6fdf6af6d39cb47
MarkoP <marko1rose@gmail.com>
2025-10-20 16:34:36 +0200

    Initial commit

---
+++ src/main/java/com/doodle/minidoodle/MinidoodleApplication.java
@@ -0,0 +1,13 @@
+package com.doodle.minidoodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinidoodleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinidoodleApplication.class, args);
+	}
+
+}
